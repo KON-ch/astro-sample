145579a37dfa37709d7846f0061ccf94f6d20413
KON-ch <k.kazuki526@gmail.com>
2023-01-08 10:21:34 +0900

    replace from header.tsx to astro

--- app/src/components/Header.tsx
+++
@@ -1,6 +0,0 @@
-import styles from "/app/src/styles/header.module.css";
-import type React from "react";
-
-export const Header: React.FC<React.PropsWithChildren> = ({ children }) => {
-  return <header className={styles.header}>{children}</header>;
-};
