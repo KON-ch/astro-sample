74d81efb3cfd3f15e34dae0f93c4ec3c6c7fcbac
KON-ch <k.kazuki526@gmail.com>
2023-01-22 06:18:53 +0000

    npx astro add preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), preact()]
 });
